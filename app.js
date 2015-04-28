044939cf1b604a3dc75c31149a9b2120c25f425c
Jacob Clark <jacob.clark@bbc.co.uk>
2015-04-28 11:19:26 +0100

    Random recipe from all seasonal recipes

--- app.js
+++ app.js
@@ -1,47 +1,59 @@
 var http 	= require('http');
-	cheerio = require('cheerio'),
-	q		= require('q');
+    cheerio = require('cheerio'),
+    q		= require('q');
 
 function makeRequest(url){
-	var deferred = q.defer();
+    var deferred = q.defer();
 
-	http.get(url, function(res) {
-		var str = "";
+    http.get(url, function(res) {
+        var str = "";
 
-		res.on('data', function (chunk) {
-			str += chunk;
-		});
+        res.on('data', function (chunk) {
+            str += chunk;
+        });
 
-		res.on('end', function () {
-			 deferred.resolve(str);
-		});
-	}).on('error', function(e) {
-		console.log("Got error: " + e.message);
-	});
+        res.on('end', function () {
+            deferred.resolve(str);
+        });
+    }).on('error', function(e) {
+        console.log("Got error: " + e.message);
+    });
 
-	return deferred.promise;
+    return deferred.promise;
 };
 
 function parseData(){
-	var deferred = q.defer();
+    var deferred = q.defer();
 
-	makeRequest('http://www.bbc.co.uk/food/recipes/search?inSeason=true').then(function(data){
-		var $ 		= cheerio.load(data),
-			json	= [];
+    makeRequest('http://www.bbc.co.uk/food/recipes/search?inSeason=true').then(function(data){
+        var $       = cheerio.load(data);
 
-		var d = $('#article-list ul li h3 a').each(function(i, link){
-			json.push({
-				title	: $(link).text(),
-				url		: $(link)[0].attribs.href
-			});
-		});
+        var d = $('#queryBox').text();
 
-		deferred.resolve(json);
-	});
+        d = d.replace(' results found', '');
 
-	return deferred.promise;
+        return Math.round(d/15);
+
+    }).then(function(pages){
+        makeRequest('http://www.bbc.co.uk/food/recipes/search?page=' + Math.floor((Math.random() * pages) + 1) + '&inSeason=true&sortBy=lastModified').then(function(data){
+
+            var $       = cheerio.load(data),
+                json    = [];
+
+            var d = $('#article-list ul li h3 a').each(function(i, link){
+                json.push({
+                    title   : $(link).text(),
+                    url     : $(link)[0].attribs.href
+                });
+            });
+
+            deferred.resolve(json);
+        });
+    });
+
+    return deferred.promise;
 };
 
 parseData().then(function(data){
-	console.log(data[Math.floor((Math.random() * 10) + 1)]);
+    console.log(data[Math.floor((Math.random() * 10) + 1)]);
 });
